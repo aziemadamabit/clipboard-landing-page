a8d6e3e6b3407fb658a9055085580e52851fd50c
aziemadamabit <aziemadama@gmail.com>
2025-07-13 10:59:21 +0200

    my files

---
+++ main.js
@@ -0,0 +1,106 @@
+let x = 15;
+x = 20;
+let y = "hi buy 55244";      
+console.log(y);
+
+const toys =[
+"balls",
+"fig",
+"balls",
+"balls",
+"balls",
+"balls",
+"balls",
+"fig",
+"balls",    
+ ];
+const ammar = {
+content: "figs",
+Number: 20,
+isbig: 20 == 5,
+};
+console.log(toys[5]);
+console.log(secondfigs.isbig);
+
+const toysinfo=[
+    {
+        content:  "balls",
+        Number:3,
+    },
+    {
+content: "figs",
+Number:5,   
+},
+{
+    content:"balls",
+    Number:1,
+},
+{
+    content:"balls",
+    Number:4,
+},
+{content: "balls",
+Number:2,
+},
+{
+  content: "balls",
+  Number: 3,
+},
+{
+    content: "balls",
+    Number: 15,
+},
+{
+    content: "figs",
+    Number: 20,
+},
+{
+    content:"balls",
+    Number: 10,
+},
+];
+/*
+console.log(ammar[0].content + 0 + "is not bigger than 5");
+console.log(ammar[1].content + 1 + "is not bigger than 5");
+console.log(ammar[2].content + 2 + "is not bigger than 5");
+console.log(ammar[3].content + 3 + "is not bigger than 5");
+console.log(ammar[4].content + 4 + "is not bigger than 5");
+console.log(ammar[5].content + 5 + "is not bigger than 5");
+console.log(ammar[6].content + 6 + "is  bigger than 5");
+console.log(ammar[7].content + 7 + "is  bigger than 5");
+console.log(ammar[8].content + 8 + "is  bigger than 5");*/
+
+for(let i = 0; i < 9; i++) {
+    console.log(ammar[i].content +" "+ ammar[i].Number);
+    console.log(ammar[i].Number > 5);
+}
+
+if(ammar[4].Number > 5){
+   console.log(ammar[4].content + " "+" is bigger than 5");
+   console.log(ammar[4].Number - 5);
+   
+}else{
+   console.log(ammar[4].content + " "+" is bigger than 5");
+   console.log(ammar[4].Number - 5);
+}
+
+function add(f, y){
+    return f - y;
+}
+const sum = add(14 , 8);
+
+console.log(sum);
+const azeim = add(15, 20);
+
+console.log(azeim);
+function isBiggerThan5(x){    
+if(ammar[4].Number > 5) {
+   console.log(x.content + " "+" is bigger than 5");
+   console.log(x.Number - 5);
+
+  }else{
+    console.log(x.content + " "+" is bigger than 5");
+   console.log(x.Number-5);
+   }
+}
+isBiggerThan5(ammar[3]);
